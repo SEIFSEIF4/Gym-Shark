f5d6bbba761282e293a1031cc3e8fa3706190c8d
FaresAlsarahin <fares.alsarahin@gmail.com>
2024-05-11 22:16:58 +0300

    cardio steps

--- src/data/cardio-exercises.ts
+++ src/data/cardio-exercises.ts
@@ -17,114 +17,233 @@ export const Exercises = [
     url: "/cardio/Burpees",
     videoLink: "https://www.youtube.com/embed/yYQL2uNmwlo?si=c4HIxRzE0mKfoSLb&amp;clip=UgkxfB83vn69nXLJoISzMoh6LsI1mYBeeGmT&amp;clipt=ENGvARixhAU",
     text: "Burpees are an exercise some of us may remember vividly from high school gym​ class. This full-body cardio exercise is memorable because it gets the heart rate up quickly.",
+    steps:[
+      "Start in a standing position with feet shoulder-width apart.",
+      "Lower into a squat and place hands on the floor in front of you.",
+      "Kick feet back to a plank position.",
+      "Perform a push-up.",
+      "Jump feet forward towards hands and explosively jump up."
+    ]
   },
   {
     exName: "Mountain climbers",
     url: "/cardio/Mountain-climbers",
     videoLink: "https://www.youtube.com/embed/hq_0YlyfqGM?si=nAYtyX7LQbLWaqEo",
     text: "Mountain climbers are an advanced, high-intensity cardio exercise that raises your heart rate and adds intensity to your workouts. This move will build leg endurance and help your agility, making it a great overall exercise.",
+    steps:[
+      "Start in a plank position with your hands directly under your shoulders.",
+      "Engage your core and drive one knee towards your chest.",
+      "Quickly switch legs, bringing the other knee towards your chest while extending the opposite leg.",
+      "Continue alternating legs in a running motion.",
+      "Maintain a steady pace and keep your hips level throughout."
+    ],
   },
   {
     exName: "Squat jumps",
     url: "/cardio/Squat-jumps",
     videoLink: "https://www.youtube.com/embed/cAINihyvQzc?si=mTw2tjSqDUQuao2v&amp;clip=Ugkxnspd8geGLR_iAly3W-56bQ_Lid12i8_X&amp;clipt=EJ6LBxj-3wo",
     text: "Squat jumps are a great way to add intensity to your workouts and raise your heart rate.5 Add this cardio exercise to the end of your regular workouts for a boost or perform them in between sets during resistance training.",
+    steps:[
+      "Start in a standing position with your feet hip-width apart.",
+      "Lower into a squat by bending your knees and pushing your hips back.",
+      "Explode upwards into a jump, extending your arms overhead.",
+      "Land softly back into the squat position with bent knees.",
+      "Repeat the jump without pausing between repetitions."
+    ],
   },
   {
     exName: "Jumping jacks",
     url: "/cardio/Jumping-jacks",
     videoLink: "https://www.youtube.com/embed/sUFZZ8tuGgA?si=7pMGOH3szEm2W6UT&amp;clip=UgkxAnVKmyeLF46C_Vi6BIYp1IFYIa3hCMHP&amp;clipt=EMChARig9gQ",
     text: "Jumping jacks are a versatile cardio exercise, ideal for spicing up workouts or boosting heart rate between sets in resistance training. They engage various muscles, enhancing cardiovascular endurance and overall fitness.",
+    steps:[
+      "Start in a standing position with your feet together and arms at your sides.",
+      "Jump your feet out to the sides while simultaneously raising your arms above your head.",
+      "Land softly on the balls of your feet with your feet shoulder-width apart and arms overhead.",
+      "Jump back to the starting position by bringing your feet together and lowering your arms to your sides.",
+      "Repeat the movement in a continuous, fluid motion."
+    ],
   },
   {
     exName: "Side Shuffle With Toe Tap",
     url: "/cardio/Side-Shuffle-With-Toe-Tap",
     videoLink: "https://www.youtube.com/embed/JQBzNDEJUlg?si=YAIQ7RGjfOJIpGC7&amp;clip=UgkxEh9R-V32P_xoJ7-JGsxAPJinT_bS5-yQ&amp;clipt=EPT-BhjU0wo",
     text: "The Side Shuffle with Toe Tap is a dynamic cardio move that combines lateral movement and balance. Start by stepping to the side with one foot, then quickly tap your other foot to meet it while keeping a continuous motion. This exercise targets your legs, glutes, and core while improving agility and coordination.",
+    steps:[
+      "Start in a standing position with your feet together.",
+      "Take a step to the side with one foot, followed by the other foot.",
+      "Perform a toe tap with the trailing leg by tapping the toes lightly on the ground.",
+      "Shuffle back in the opposite direction, leading with the other foot.",
+      "Repeat the side shuffle with toe taps in a continuous motion."
+    ],
   },
   {
     exName: "Side Lunges",
     url: "/cardio/Side-Lunge",
     videoLink: "https://www.youtube.com/embed/X4Lqkmo28wY?si=QgNumnnzxfgwnhll&amp;clip=UgkxwvGlY6k4F0cJKhSZ3GQ08WSGPGwie-VT&amp;clipt=EI7yBRjtxgk",
     text: "If you want a great whole-body cardio exercise that raises your heart rate quickly, side-to-side lunges will do the trick. You can perform this exercise with a jump to add more intensity, but doing it without a jump will also work.",
+    steps:[
+      "Start in a standing position with your feet shoulder-width apart.",
+      "Take a step to the side with one foot, keeping the other foot planted.",
+      "Bend the knee of the stepping leg, lowering your body into a lunge position.",
+      "Keep your chest up and core engaged throughout the movement.",
+      "Push through the heel of the lunging leg to return to the starting position. Alternate sides."
+    ],
   },
   {
     exName: "Squat to Side Lunge",
     url: "/cardio/Squat-to-Side-Lunge",
     videoLink: "https://www.youtube.com/embed/Hn9zUpS-fJA?si=tD3K-whkiTXcci8j&amp;clip=UgkxlepEj3VDnTSjRgdyZfwhCW3Nu6OOOjE2&amp;clipt=EOeFExjH2hY",
     text: "The Squat to Side Lunge combines a squat and lateral lunge for a full lower-body workout. Start with feet hip-width apart, squat, then step to the side into a lunge. Return to squat and repeat on the other side. It targets quads, hamstrings, glutes, and improves overall lower-body strength and stability.",
+    steps:[
+      "Start in a standing position with your feet shoulder-width apart.",
+      "Lower into a squat by bending your knees and pushing your hips back.",
+      "Push through your heels to stand up and step to the side into a lunge position.",
+      "Keep your chest up and core engaged during the side lunge.",
+      "Return to the squat position and repeat on the opposite side."
+    ],
   },
   {
     exName: "Vertical Jump",
     url: "/cardio/Vertical-Jump",
     videoLink: "https://www.youtube.com/embed/sUFZZ8tuGgA?si=Ztj9KyogcqJ4NFd_&amp;clip=Ugkx2NFNOuIETySgqYUphysSNy-sP9Ik6yBE&amp;clipt=EPafLRjspTA",
     text: "Vertical jumps are explosive plyometric exercises that boost leg strength and power. Start in a squat, then jump as high as possible, reaching upward. Land softly and repeat. Great for improving vertical leap and overall lower-body strength.",
+    steps:[
+      "Start in a standing position with your feet hip-width apart.",
+      "Bend your knees and lower into a squat position, keeping your back straight.",
+      "Explode upwards with maximum effort, extending your arms overhead.",
+      "Straighten your legs as you jump up.",
+      "Land softly on the balls of your feet and absorb the impact by bending your knees."
+    ],
   },
   {
     exName: "Pulsing Lunge",
     url: "/cardio/Pulsing-Lunge",
     videoLink: "https://www.youtube.com/embed/REFSXurvDTM?si=MKj2e_LmGW10LTFj&amp;clip=UgkxdS8EexLVcAAEjtaVVhisfBz4ebUoX6EK&amp;clipt=ENv1Axiw6QY",
     text: "Pulsing Lunges are dynamic leg exercises that target muscles with short up-and-down movements. Great for building leg strength and endurance.",
+    steps:[
+      "Start in a standing position with your feet hip-width apart.",
+      "Step one foot back into a lunge position, bending both knees to 90-degree angles.",
+      "Lower your body down until your front thigh is parallel to the ground.",
+      "Pulse up and down slightly, moving your body a few inches up and down while maintaining the lunge position.",
+      "Switch legs and repeat the pulsing motion on the other side."
+    ],
   },
   {
     exName: "Jogging with high knees",
     url: "/cardio/Jogging-with-high-knees",
     videoLink: "https://www.youtube.com/embed/q2NZyW5EP5A?si=ZyNslQWZlz26FyZ2&amp;clip=Ugkx3OO4e6yYaE2vZNyES48P4bWrX8EPsPtw&amp;clipt=EK-0BBiPiQg",
     text: "To add intensity to jogging in place, try lifting your knees high. Bring your knees to hip height to engage your core, quads, and hip flexors.",
+    steps:[
+      "Start in a standing position with your feet hip-width apart and arms at your sides.",
+      "Begin jogging in place, lifting your knees as high as possible towards your chest with each step.",
+      "Engage your core muscles to lift your knees higher and maintain balance.",
+      "Keep your upper body upright and your chest lifted throughout the movement.",
+      "Continue jogging with high knees at a steady pace."
+    ],
   },
   {
     exName: "Jogging in place",
     url: "/cardio/Jogging-in-place",
     videoLink: "https://www.youtube.com/embed/q2NZyW5EP5A?si=e0iOV95IZcKGpXac&amp;clip=UgkxrBAue9m1fpEBxbYnHP-HGp-6N_ho_fp4&amp;clipt=EPiVARjCqAQ",
     text: "Jogging in place is a simple indoor cardio option to elevate your heart rate. While it lacks the intensity of outdoor jogging due to the absence of forward motion and wind resistance, you can increase the challenge by using your arms and adding movement around your home or stairs.",
+    steps:[
+      "Start in a standing position with your feet hip-width apart and arms at your sides.",
+      "Lift your right knee up towards your chest while simultaneously raising your left arm.",
+      "Quickly switch legs and arms, bringing your left knee up and your right arm up.",
+      "Continue alternating between lifting your knees and arms in a running motion while staying in place.",
+      "Maintain a steady pace and keep your core engaged throughout."
+    ],
   },
   {
     exName: "Jump kick lunge",
     url: "/cardio/Jump-kick-lunge",
     videoLink: "https://www.youtube.com/embed/Y-wqbrfiHMk?si=26ruZPZ2BKKTOlyW",
     text: "This is a great cardio exercise to raise your heart rate with no equipment. It is low impact, but that doesn't mean low intensity. Adding a low lunge while touching the floor will engage your glutes and thighs and help to raise your heart rate",
+    steps:[
+      "Start in a standing position with your feet shoulder-width apart.",
+      "Step one foot back into a lunge position, bending both knees to 90-degree angles.",
+      "Push through the front heel and explode upwards, lifting the back knee towards your chest.",
+      "Simultaneously kick the leg that was in the lunge position forward.",
+      "Land softly back into the lunge position and repeat the movement on the opposite side."
+    ],
   },
   {
     exName: "Speed skaters",
     url: "/cardio/Speed-skaters",
     videoLink: "https://www.youtube.com/embed/8tRI-mNe-9w?si=kB3yrZVZsWP5GrhB&amp;clip=UgkxC6g5APQQBM_j99rFKQb--09E2-qgk5Oi&amp;clipt=EIfqKBi7lis",
     text: "Speed skaters are great for getting your heart rate up and working the body with lateral movement, something we often don't spend much time doing. ",
+    steps:[
+      "Place your feet about hip-width apart, and squat low enough that you can put your hands on the floor in front of you.",
+      "Explode and jump up, using your glutes, quads, and hamstrings to generate power.",
+      "Tap your heels together as you jump and take the hands behind your head or up in the air.",
+      "Land with bent knees to protect your joints and get into your squat to prepare for the next jump.",
+      "Repeat 10 to 20 frog jumps. Rest and repeat if desired.",
+    ],
   },
   {
     exName: "Flutter Kick",
     url: "/cardio/Flutter-Kick",
     videoLink: "https://www.youtube.com/embed/8AvPJAimmeg?si=cEYMUuPnnFlLi4sW&amp;clip=UgkxAjRU9LIXYc3u11OZ6VlkH8layKcHY-0t&amp;clipt=EPDkFhj19Bk",
     text: "Flutter kicks are a core-strengthening exercise where you lie on your back, lift your legs slightly off the ground, and kick them up and down in quick, alternating motions. This move targets your lower abs and hip flexors, helping improve core stability and endurance.",
+    steps:[
+      "Start in a standing position with your feet hip-width apart and arms at your sides.",
+      "Jump to the right, landing on your right foot with your left leg crossed behind your right leg.",
+      "Simultaneously swing your left arm across your body and reach towards your right foot.",
+      "Jump to the left, landing on your left foot with your right leg crossed behind your left leg.",
+      "Swing your right arm across your body and reach towards your left foot."
+    ],
   },
   {
     exName: "Jump Lunge to Jump Squat",
     url: "/cardio/Jump-Lunge-to-Jump-Squat",
     videoLink: "https://www.youtube.com/embed/0VfCpXJQRYg?si=RHOtIZKXsaPMqMo5&amp;clip=Ugkx44K9cB66e4dNvk7YbNYttHx90Niexh6j&amp;clipt=EOy_DxjjghM",
     text: "The Jump Lunge to Jump Squat is a dynamic combo that targets your legs and boosts cardio fitness. Start with alternating jump lunges, then transition into jump squats for a powerful lower-body workout.",
+    steps:[
+      "Start in a standing position with your feet hip-width apart.",
+      "Lower into a lunge by stepping one foot back and bending both knees to 90-degree angles.",
+      "Push through your front heel and jump explosively, switching legs mid-air to land in a lunge on the opposite side.",
+      "From the lunge position, immediately jump up into a squat position, bending your knees and lowering your hips.",
+      "Explode upward from the squat, extending your legs and arms overhead."
+    ],
   },
   {
     exName: "Plusing Side Plank",
     url: "/cardio/Plusing-Side-Plank",
     videoLink: "https://www.youtube.com/embed/0VfCpXJQRYg?si=GxeJZj3dH0KsKj3Q&amp;clip=UgkxFQ-ctB_cuUbajcHHJm-R8Xnmlk5c62-u&amp;clipt=EMXnFhjJkxo",
     text: "The Plank Side Pulse is a challenging core exercise that involves balancing on one forearm and the side of your feet while pulsing your hips up and down slightly. This move targets your obliques and improves core stability and strength.",
+    steps:[
+      "Start by lying on your side with your forearm on the ground and your elbow directly below your shoulder.",
+      "Lift your hips off the ground, creating a straight line from your head to your heels.",
+      "Lower your hips slightly toward the ground, then lift them back up to the starting position.",
+      "Continue pulsing your hips up and down in a controlled motion.",
+      "Switch sides and repeat the pulsing side plank on the opposite side."
+    ],
   },
   {
     exName: "Star Jump",
     url: "/cardio/Star-Jump",
     videoLink: "https://www.youtube.com/embed/5AcCoxTIPqE?si=axW3BsxuVLkYOVqn&amp;clip=Ugkx2KlGrDf-YRmm-zF_QQQjIXGa9V056ppK&amp;clipt=ENeYARi5mQQ",
     text: "The Star Jump is a dynamic plyometric exercise where you jump explosively, spreading your arms and legs out like a star. It works multiple muscles and boosts cardiovascular endurance.",
+    steps:[
+      "Start in a standing position with your feet together and arms at your sides.",
+      "Bend your knees slightly and jump up explosively.",
+      "Simultaneously spread your legs wide and extend your arms out to the sides and overhead, forming a star shape.",
+      "Land softly back on the balls of your feet with your knees bent.",
+      "Repeat the star jump in a fluid, continuous motion."
+    ],
   },
   {
     exName: "Pendulum Lunge",
     url: "/cardio/Pendulum-Lunge",
     videoLink: "https://www.youtube.com/embed/Hn9zUpS-fJA?si=3A_3SKwpt6SqH5zh&amp;clip=UgkxIQNfXuPi5pdZ1hDEdwF1VzxnRsOYfoQt&amp;clipt=EKTvBhiExAo",
     text: "The Pendulum Lunge is a dynamic leg exercise involving stepping into a lunge and swinging the back leg forward into a kick before returning. It targets multiple leg muscles and improves balance.",
-    // steps:[
-    //   "Start in a standing position with feet hip-width apart.",
-    //   "Step forward into a lunge, ensuring proper alignment of front knee and back knee.",
-    //   "Swing back leg forward into a front kick position.",
-    //   "Return to lunge position with back leg behind.",
-    //   "Push through front heel to return to standing. Repeat on alternate sides.",
-    // ],
+    steps:[
+      "Start in a standing position with feet hip-width apart.",
+      "Step forward into a lunge, ensuring proper alignment of front knee and back knee.",
+      "Swing back leg forward into a front kick position.",
+      "Return to lunge position with back leg behind.",
+      "Push through front heel to return to standing. Repeat on alternate sides.",
+    ],
   },
 ];
