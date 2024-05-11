1e13bcbda73a49526cd04d3af78c29776b2629b0
FaresAlsarahin <fares.alsarahin@gmail.com>
2024-05-12 02:58:24 +0300

    modified

--- src/app/cardio/page.tsx
+++ src/app/cardio/page.tsx
@@ -6,23 +6,25 @@ import IFrame from "@/components/IFrame";
 
 export default function Cardio() {
   return (
-    <div className="gap-5 grid grid-cols-2 sm:grid-cols-3 p-20 ">
+    <div className="gap-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 p-20 ">
       {Exercises.map((item, index) => (
         <Card shadow="md" key={index}>
           <CardBody className="overflow-visible p-0">
             <Suspense fallback={<p>Loading video...</p>}>
-            <Image
-              shadow="sm"
-              radius="lg"
-              width="100%"
-              alt={item.exName}
-              className="w-full object-cover h-[250px]"
-              src={item.thumbnail}
-            />
+            <div className="sm:max-w-[253px] md:max-w-none">
+              <Image
+                shadow="sm"
+                radius="lg"
+                width="100%"
+                alt={item.exName}
+                className="w-full object-none md:object-cover h-[200px]"
+                src={item.thumbnail}
+              />
+            </div>
             </Suspense>
           </CardBody>
           <CardFooter className="text-small flex-col gap-1 ">
-            <p className="font-bold text-md">{item.exName}</p>
+            <p className="font-bold text-lg hover:text-primary">{item.exName}</p>
           </CardFooter>
         </Card>
       ))}
