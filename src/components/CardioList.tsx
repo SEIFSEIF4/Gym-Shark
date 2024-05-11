32f040baad66d974299e9811caa205664b882161
FaresAlsarahin <fares.alsarahin@gmail.com>
2024-05-11 21:40:08 +0300

    Cardio exercises modified

--- src/components/CardioList.tsx
+++ src/components/CardioList.tsx
@@ -5,7 +5,7 @@ import CardioExercisesHeader from "./CardioExercisesHeader";
 
 export default function CardioExercisesPage() {
   return (
-    <div className="w-4/6 flex flex-col m-auto mt-10">
+    <div className="flex flex-col m-auto mt-10">
       <CardioExercisesHeader />
       <div className="flex flex-col lg:flex-row gap-4 mt-5 w-11/12">
         <ExercisesList />
