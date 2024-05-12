06dc4b5dfae0b84c17ee17be8f680a5f272f65fc
Seif Elesllam <seifsaif151@gmail.com>
2024-05-12 04:27:14 +0300

    fix files

---
+++ src/app/store/page.tsx
@@ -0,0 +1,12 @@
+import Newest from "@/components/Newest";
+import Hero from "@/components/StoreHero";
+import React from "react";
+
+export default function Store() {
+  return (
+    <div>
+      <Hero />
+      <Newest />
+    </div>
+  );
+}
