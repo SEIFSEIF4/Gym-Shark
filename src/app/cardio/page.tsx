7e9fcfc89c618436524430fae097a0a6619ff54d
Seif Elesllam <seifsaif151@gmail.com>
2024-05-12 03:47:34 +0300

    modified

--- src/app/cardio/page.tsx
+++ src/app/cardio/page.tsx
@@ -4,6 +4,7 @@ import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
 import { Exercises } from "@/data/cardio-exercises";
 import IFrame from "@/components/IFrame";
 import MainNavbar from "@/components/MainNavbar";
+import Link from "next/link";
 
 export default function Cardio() {
   return (
@@ -11,27 +12,29 @@ export default function Cardio() {
       <MainNavbar />
       <div className="gap-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 p-20 ">
         {Exercises.map((item, index) => (
-          <Card shadow="md" key={index}>
-            <CardBody className="overflow-visible p-0">
-              <Suspense fallback={<p>Loading video...</p>}>
-                <div className="sm:max-w-[253px] md:max-w-none">
-                  <Image
-                    shadow="sm"
-                    radius="lg"
-                    width="100%"
-                    alt={item.exName}
-                    className="w-full object-none md:object-cover h-[200px]"
-                    src={item.thumbnail}
-                  />
-                </div>
-              </Suspense>
-            </CardBody>
-            <CardFooter className="text-small flex-col gap-1 ">
-              <p className="font-bold text-lg hover:text-primary">
-                {item.exName}
-              </p>
-            </CardFooter>
-          </Card>
+          <Link href={item.url} key={index}>
+            <Card shadow="md">
+              <CardBody className="overflow-visible p-0">
+                <Suspense fallback={<p>Loading video...</p>}>
+                  <div className="sm:max-w-[253px] md:max-w-none">
+                    <Image
+                      shadow="sm"
+                      radius="lg"
+                      width="100%"
+                      alt={item.exName}
+                      className="w-full object-none md:object-cover h-[200px]"
+                      src={item.thumbnail}
+                    />
+                  </div>
+                </Suspense>
+              </CardBody>
+              <CardFooter className="text-small flex-col gap-1 ">
+                <p className="font-bold text-lg hover:text-primary">
+                  {item.exName}
+                </p>
+              </CardFooter>
+            </Card>
+          </Link>
         ))}
       </div>
     </>
