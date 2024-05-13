a8fc2cf56f68f45e67fce3258536ffc95750bad5
Seif Elesllam <seifsaif151@gmail.com>
2024-05-13 07:02:13 +0300

    modified

---
+++ src/data/equipments.ts
@@ -0,0 +1,161 @@
+const Product1 = {
+  _id: "1",
+  slug: "Back-Machine",
+  name: "A6XL Chest – Back Machine",
+  description:
+    "A6XL Chest – Back Machine METAL COLOR: Black RAL9005 LEATHER COLOR: Black",
+  price: 8000,
+  categoryName: "equipments",
+  images: [
+    "https://www.gymequip.eu/wp-content/uploads/2024/01/chest-back-machine.jpg",
+    "https://www.gymequip.eu/wp-content/uploads/2024/01/chest-back-machine-1-150x150.jpg",
+    "https://www.gymequip.eu/wp-content/uploads/2024/01/chest-back-machine-2-450x450.jpg",
+  ],
+  rate: 4.5,
+  ratings: 10,
+  shipping: "2-4 days Shipping",
+};
+
+const Product2 = {
+  _id: "2",
+  slug: "Smith-Machine",
+  name: "B5X Smith Machine",
+  description: "B5X Smith Machine METAL COLOR: Black RAL9005",
+  price: 5000,
+  categoryName: "equipments",
+  images: [
+    "https://www.gymequip.eu/wp-content/uploads/2015/10/Smith-Machine-2.jpg",
+    "https://www.gymequip.eu/wp-content/uploads/2015/10/Smith-Machine-2-1-150x150.jpg",
+    "https://www.gymequip.eu/wp-content/uploads/2015/10/Smith-Machine-3.jpg",
+  ],
+  rate: 4.5,
+  ratings: 10,
+  shipping: "2-4 days Shipping",
+};
+const Product3 = {
+  _id: "3",
+  slug: "Adjustable-Pulley",
+  name: "C2XX Single Stack Adjustable Pulley",
+  description: "C2XX Single Stack Adjustable Pulley METAL COLOR: Black RAL9005",
+  price: 9000,
+  categoryName: "equipments",
+  images: [
+    "https://www.gymequip.eu/wp-content/uploads/2023/12/Single-Stack-Adjustable-Pulley-.jpg",
+    "https://www.gymequip.eu/wp-content/uploads/2023/12/Single-Stack-Adjustable-Pulley-3--450x450.jpg",
+    "https://www.gymequip.eu/wp-content/uploads/2023/12/Single-Stack-Adjustable-Pulley-5--450x450.jpg",
+  ],
+  rate: 4.5,
+  ratings: 10,
+  shipping: "2-4 days Shipping",
+};
+const Product4 = {
+  _id: "4",
+  slug: "Squat-Machine",
+  name: "R3 Pendulum Squat Machine",
+  description:
+    "R3 Pendulum Squat Machine METAL COLOR: Black RAL9005 LEATHER COLOR: Black ",
+  price: 2500,
+  categoryName: "equipments",
+  images: [
+    "https://www.gymequip.eu/wp-content/uploads/2024/01/Pendulum-Squat-Machine-.jpg",
+    "https://www.gymequip.eu/wp-content/uploads/2024/01/Pendulum-Squat-Machine-6--450x450.jpg",
+    "https://www.gymequip.eu/wp-content/uploads/2024/01/Pendulum-Squat-Machine-4-.jpg",
+  ],
+  rate: 4.5,
+  ratings: 10,
+  shipping: "2-4 days Shipping",
+};
+const Product5 = {
+  _id: "5",
+  slug: "Curl-Machine",
+  name: "M8XX Leg Extension Leg Curl Machine",
+  description: "M8XX Leg Extension Leg Curl Machine",
+  price: 7500,
+  categoryName: "equipments",
+  images: [
+    "https://www.gymequip.eu/wp-content/uploads/2020/04/Leg-Extension-Leg-Curl-Machine-.jpg",
+    "https://www.gymequip.eu/wp-content/uploads/2020/04/Leg-Extension-Leg-Curl-Machine-3--450x450.jpg",
+    "https://www.gymequip.eu/wp-content/uploads/2020/04/Leg-Extension-Leg-Curl-Machine-4-.jpg",
+  ],
+  rate: 4.1,
+  ratings: 10,
+  shipping: "2-4 days Shipping",
+};
+const Product6 = {
+  _id: "6",
+  slug: "Hip-Thrust-Machine",
+  name: "Z16X Hip Thrust Machine",
+  description: "M8XX Leg Extension Leg Curl Machine",
+  price: 6500,
+  categoryName: "equipments",
+  images: [
+    "https://www.gymequip.eu/wp-content/uploads/2023/09/Hip-Thrust-Machine-Z16X-.jpg",
+    "https://www.gymequip.eu/wp-content/uploads/2023/09/Hip-Thrust-Machine-Z16X-2-1-450x450.jpg",
+    "https://www.gymequip.eu/wp-content/uploads/2023/09/Hip-Thrust-Machine-Z16X-4-450x450.jpg",
+  ],
+  rate: 4.4,
+  ratings: 50,
+  shipping: "2-4 days Shipping",
+};
+const Product7 = {
+  _id: "7",
+  slug: "Adductor-Machine",
+  name: "V3 Abductor and Adductor Machine",
+  description:
+    "V3 Abductor and Adductor Machine METAL COLOR: Black RAL9005 LEATHER COLOR: Black",
+  price: 7500,
+  categoryName: "equipments",
+  images: [
+    "https://www.gymequip.eu/wp-content/uploads/2024/02/abductor-and-adductor-machine-.jpg",
+    "https://www.gymequip.eu/wp-content/uploads/2024/02/abductor-and-adductor-machine-3-1-1-450x450.jpg",
+    "https://www.gymequip.eu/wp-content/uploads/2024/02/abductor-and-adductor-machine-4--450x450.jpg",
+  ],
+  rate: 4.9,
+  ratings: 90,
+  shipping: "2-4 days Shipping",
+};
+const Product8 = {
+  _id: "8",
+  slug: "Squat-Machine",
+  name: "R3 Pendulum Squat Machine",
+  description:
+    "R3 Pendulum Squat Machine METAL COLOR: Black RAL9005 LEATHER COLOR: Black",
+  price: 7900,
+  categoryName: "equipments",
+  images: [
+    "https://www.gymequip.eu/wp-content/uploads/2024/01/Pendulum-Squat-Machine-.jpg",
+    "https://www.gymequip.eu/wp-content/uploads/2024/01/Pendulum-Squat-Machine-2--450x450.jpg",
+    "https://www.gymequip.eu/wp-content/uploads/2024/01/Pendulum-Squat-Machine-3--450x450.jpg",
+  ],
+  rate: 4.9,
+  ratings: 30,
+  shipping: "2-4 days Shipping",
+};
+const Product9 = {
+  _id: "9",
+  slug: "Dip-Attachment",
+  name: "Half Rack + Dip Attachment",
+  description: "Half Rack + Dip Attachment",
+  price: 7500,
+  categoryName: "equipments",
+  images: [
+    "https://www.gymequip.eu/wp-content/uploads/2023/12/Half-Rack-Dip-Attachment.jpg",
+    "https://www.gymequip.eu/wp-content/uploads/2023/12/Half-Rack-Dip-Attachment-2-450x450.jpg",
+    "https://www.gymequip.eu/wp-content/uploads/2023/12/Half-Rack-Dip-Attachment-3-450x563.jpg",
+  ],
+  rate: 4.2,
+  ratings: 10,
+  shipping: "2-4 days Shipping",
+};
+
+export const EquipmentsProducts = [
+  Product1,
+  Product2,
+  Product3,
+  Product4,
+  Product5,
+  Product6,
+  Product7,
+  Product8,
+  Product9,
+];
--- src/data/main-products.ts
+++ src/data/main-products.ts
@@ -1,4 +1,4 @@
-import { EquipmentsProducts } from "@/data/equipments-products";
+import { EquipmentsProducts } from "@/data/equipments";
 import { SupplementsProducts } from "@/data/supplements-products";
 import { AccessoriesProducts } from "@/data/accessories-products";
 
