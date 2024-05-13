b918f73d23bd483f6a10c87847809b67c782f032
Seif Elesllam <seifsaif151@gmail.com>
2024-05-13 04:26:56 +0300

    modified

--- src/app/store/product/[slug]/page.tsx
+++ src/app/store/product/[slug]/page.tsx
@@ -7,6 +7,7 @@ import { Button } from "@/components/ui/button";
 import { MainProducts } from "@/data/main-products";
 import Navbar from "@/components/Navbar";
 import AddToBag from "@/components/AddToBag";
+import NewestMeals from "@/components/NewestMeals";
 
 export default async function ProductPage({
   params,
@@ -35,76 +36,81 @@ export default async function ProductPage({
           </div>
         </div>
       ) : (
-        <div className="bg-card h-[88vh]">
-          <div className="mx-auto max-w-screen-xl px-4 md:px-8 ">
-            <div className="grid gap-8 md:grid-cols-2">
-              <ImageGallery images={data.images} />
-              <div className="md:py-8 ">
-                <div className="mb-2 md:mb-3">
-                  <span className="mb-0.5 inline-block text-gray-400">
-                    {data.categoryName}
-                  </span>
-                  <h2 className="text-2xl font-bold text-foreground lg:text-3xl">
-                    {data.name}
-                  </h2>
-                </div>
-
-                <div className="mb-6 flex items-center gap-3 md:mb-10">
-                  <Button className="rounded-full gap-x-2 ">
-                    <span className="text-sm">{data.rate}</span>
-                    <Star className="h-5 w-5 text-yellow-500 stroke-yellow-500" />
-                  </Button>
-
-                  <span className="text-sm text-gray-400 transition duration-100">
-                    {data.ratings} Ratings
-                  </span>
-                </div>
-
-                <div className="mb-4">
-                  <div className="flex items-end gap-2">
-                    <span className="text-xl font-bold text-foreground md:text-2xl">
-                      {data.price}TL
+        <>
+          <div className="bg-card main-height flex-col flex justify-center items-start py-16">
+            <div className="mx-auto max-w-screen-xl px-4 md:px-8 ">
+              <div className="grid gap-8 md:grid-cols-2">
+                <ImageGallery images={data.images} />
+                <div className="md:py-8 ">
+                  <div className="mb-2 md:mb-3">
+                    <span className="mb-0.5 inline-block text-gray-400">
+                      {data.categoryName}
                     </span>
-                    <span className="mb-0.5 text-red-500 line-through">
-                      {data.price + 30}TL
+                    <h2 className="text-2xl font-bold text-foreground lg:text-3xl">
+                      {data.name}
+                    </h2>
+                  </div>
+
+                  <div className="mb-6 flex items-center gap-3 md:mb-10">
+                    <Button className="rounded-full gap-x-2 ">
+                      <span className="text-sm">{data.rate}</span>
+                      <Star className="h-5 w-5 text-yellow-500 stroke-yellow-500" />
+                    </Button>
+
+                    <span className="text-sm text-gray-400 transition duration-100">
+                      {data.ratings} Ratings
                     </span>
                   </div>
 
-                  <span className="text-sm text-gray-400">{data.shipping}</span>
+                  <div className="mb-4">
+                    <div className="flex items-end gap-2">
+                      <span className="text-xl font-bold text-foreground md:text-2xl">
+                        {data.price}TL
+                      </span>
+                      <span className="mb-0.5 text-red-500 line-through">
+                        {data.price + 30}TL
+                      </span>
+                    </div>
+
+                    <span className="text-sm text-gray-400">
+                      {data.shipping}
+                    </span>
+                  </div>
+
+                  <div className="mb-6 flex items-center gap-2 text-gray-400">
+                    <Truck className="w-6 h-6" />
+                    <span className="text-sm">2-4 Day Shipping</span>
+                  </div>
+
+                  <div className="flex gap-2.5">
+                    <AddToBag
+                      currency="TL"
+                      description={data.description}
+                      image={data.images[0]}
+                      name={data.name}
+                      price={data.price}
+                      key={data._id}
+                    />
+
+                    <CheckoutNow
+                      currency="TL"
+                      description={data.description}
+                      image={data.images[0]}
+                      name={data.name}
+                      price={data.price}
+                      key={data._id}
+                    />
+                  </div>
+
+                  <p className="mt-12 text-base text-gray-400 tracking-wide">
+                    {data.description}
+                  </p>
                 </div>
-
-                <div className="mb-6 flex items-center gap-2 text-gray-400">
-                  <Truck className="w-6 h-6" />
-                  <span className="text-sm">2-4 Day Shipping</span>
-                </div>
-
-                <div className="flex gap-2.5">
-                  <AddToBag
-                    currency="TL"
-                    description={data.description}
-                    image={data.images[0]}
-                    name={data.name}
-                    price={data.price}
-                    key={data._id}
-                  />
-
-                  <CheckoutNow
-                    currency="TL"
-                    description={data.description}
-                    image={data.images[0]}
-                    name={data.name}
-                    price={data.price}
-                    key={data._id}
-                  />
-                </div>
-
-                <p className="mt-12 text-base text-gray-400 tracking-wide">
-                  {data.description}
-                </p>
               </div>
             </div>
+            <NewestMeals />
           </div>
-        </div>
+        </>
       )}
     </>
   );
