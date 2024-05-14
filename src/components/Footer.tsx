2bdfb9bc684ac21e53b7dd278ce27ff9fc8b3e1b
FaresAlsarahin <fares.alsarahin@gmail.com>
2024-05-14 18:19:19 +0300

    Done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,13 @@
 const Footer = () => {
+const currentYear = new Date().getFullYear();
   return (
-    <footer className="bg-zinc-800 text-gray-300 py-8">
+    <footer className="bg-zinc-900 text-gray-300 py-8">
       <div className="container mx-auto">
-        <div className="flex flex-wrap">
+        <div className="flex flex-wrap md:gap-40">
           <div className="w-full md:w-1/2 md:pr-6 mb-6 md:mb-0">
-            <h6 className="text-white text-lg uppercase mb-2">About</h6>
-            <p className="text-justify">
-              Our gym, <strong>GymFlex</strong>, believes in making fitness
+            <h6 className="text-white text-lg uppercase mb-2 ">About</h6>
+            <p className="text-justify pr-8">
+              Our gym, <strong>GymShark</strong>, believes in making fitness
               simple and accessible to everyone. We are dedicated to assisting
               fitness enthusiasts in achieving their goals by providing
               effective workout routines and expert guidance. GymFlex focuses on
@@ -19,50 +20,23 @@ const Footer = () => {
           </div>
 
           <div className="w-full md:w-1/4">
-            <h6 className="text-white text-lg uppercase mb-2">Categories</h6>
-            <ul className="footer-links">
-              <li>
-                <a href="">NextJs</a>
-              </li>
-              <li>
-                <a href="">UI Design</a>
-              </li>
-              <li>
-                <a href="">JavaScript</a>
-              </li>
-              <li>
-                <a href="">Tailwind</a>
-              </li>
-              <li>
-                <a href="">Html</a>
-              </li>
-              <li>
-                <a href="">Css</a>
-              </li>
-            </ul>
-          </div>
-
-          <div className="w-full md:w-1/4">
             <h6 className="text-white text-lg uppercase mb-2">Quick Links</h6>
             <ul className="footer-links">
               <li>
-                <a href="http://scanfcode.com/about/">About Us</a>
+                <a href="/">Home</a>
               </li>
               <li>
-                <a href="http://scanfcode.com/contact/">Contact Us</a>
+                <a href="/cardio">Cardio</a>
               </li>
               <li>
-                <a href="http://scanfcode.com/contribute-at-scanfcode/">
-                  Contribute
+                <a href="/meals-plan">
+                  Meals Plan
                 </a>
               </li>
               <li>
-                <a href="http://scanfcode.com/privacy-policy/">
-                  Privacy Policy
+                <a href="/store">
+                  Store
                 </a>
-              </li>
-              <li>
-                <a href="http://scanfcode.com/sitemap/">Sitemap</a>
               </li>
             </ul>
           </div>
@@ -71,37 +45,12 @@ const Footer = () => {
         <div className="flex flex-wrap items-center justify-between">
           <div className="w-full md:w-2/3">
             <p className="text-center md:text-left text-sm">
-              &copy; 2024 All Rights Reserved by{" "}
+              &copy; {currentYear} All Rights Reserved by{" "}
               <a href="#" className="text-blue-500">
-                Scanfcode
+                GymShark
               </a>
               .
             </p>
-          </div>
-
-          <div className="w-full md:w-1/3 mt-4 md:mt-0">
-            <ul className="flex justify-end">
-              <li className="mx-2">
-                <a className="social-icon" href="#">
-                  <i className="fab fa-facebook"></i>
-                </a>
-              </li>
-              <li className="mx-2">
-                <a className="social-icon" href="#">
-                  <i className="fab fa-twitter"></i>
-                </a>
-              </li>
-              <li className="mx-2">
-                <a className="social-icon" href="#">
-                  <i className="fab fa-dribbble"></i>
-                </a>
-              </li>
-              <li className="mx-2">
-                <a className="social-icon" href="#">
-                  <i className="fab fa-linkedin"></i>
-                </a>
-              </li>
-            </ul>
           </div>
         </div>
       </div>
